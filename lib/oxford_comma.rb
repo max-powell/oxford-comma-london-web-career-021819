2409db9f61b047697ed6d6e522c8675e066f104e
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-29 20:26:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,6 @@
 def oxford_comma(array)
-  return array[0] if array.length ==1
+  return array[0] if array.length == 1
+  return "#{array[0]} and #{array[1]}" if array.length == 2
   ending = "and " + array.pop
   array << ending
   array.join(", ")
